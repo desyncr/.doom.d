b198dd99e5a2fdbc275b4082a601e88b3a3c160f
desyncr <des@riseup.net>
2022-08-11 19:27:47 +0100

    Update compiled configuration

--- config.el
+++ config.el
@@ -15,73 +15,54 @@
                     :background nil
                     :italic t)))
   :config
-  (global-blamer-mode 1))
-;; https://github.com/bbatsov/super-save
+  (global-blamer-mode 0))
+(use-package better-jumper
+  :ensure t
+  :config
+  (better-jumper-mode +1))
+(with-eval-after-load 'evil-maps
+  (define-key evil-motion-state-map (kbd "C-o") 'better-jumper-jump-backward)
+  (define-key evil-motion-state-map (kbd "<C-i>") 'better-jumper-jump-forward))
 (use-package super-save
   :ensure t
   :config
   (super-save-mode +1))
-;; Comment or uncomment region with M-/
-(map! :ne "M-/" #'comment-or-uncomment-region)
-
 (map! :leader :desc "Open Dashboard" "d" #'+doom-dashboard/open)
-
-;; Search with deadgrep
-(map! :ne "SPC r" #'deadgrep)
-(use-package multi-magit
-  :bind ("C-x G" . multi-magit-status)
-  :ensure nil
-  :config (progn
-            (unless magit-repository-directories
-              (setq magit-repository-directories '(("~/sys-vagrant/code/" . 2))))
-            (magit-add-section-hook 'magit-status-sections-hook
-                                    'multi-magit-insert-repos-overview
-                                    nil t)))
-
-
-(use-package which-key
-  :ensure t
-  :config
-  (setq which-key-idle-delay 1.0 ;; Default is 1.0
-      which-key-idle-secondary-delay 0.5) ;; Default is nil
-)
+(map! :ne "M-/" #'comment-or-uncomment-region)
+(map! "s-b" #'ido-switch-buffer)
+(map! "s-p" #'+treemacs/toggle)
+(map! "s-§" #'+treemacs/toggle)
+(map! "s-s" #'save-buffer)
+(map! "s-f" #'+default/search-project)
 (after! projectile
    (setq
         projectile-project-search-path '("~/sys-vagrant/code/")
    )
 )
-
-(setq doom-modeline-enable-word-count t)
 (use-package treemacs
   :ensure t
   :config
   (setq treemacs-is-never-other-window t))
-
 (use-package treemacs-projectile
   :after (treemacs projectile)
   :ensure t)
-
 (use-package treemacs-magit
   :after (treemacs magit)
   :ensure t)
-
 (use-package treemacs-persp ;;treemacs-perspective if you use perspective.el vs. persp-mode
   :after (treemacs persp-mode) ;;or perspective vs. persp-mode
   :ensure t
   :config (treemacs-set-scope-type 'Perspectives))
-
-
-;; https://github.com/emacs-lsp/lsp-treemacs/issues/89
-(with-eval-after-load 'lsp-treemacs
-          (doom-themes-treemacs-config))
-(define-key evil-normal-state-map (kbd "C-k") 'spatial-navigate-backward-vertical-box)
-(define-key evil-normal-state-map (kbd "C-j") 'spatial-navigate-forward-vertical-box)
-(define-key evil-normal-state-map (kbd "C-h") 'spatial-navigate-backward-horizontal-box)
-(define-key evil-normal-state-map (kbd "C-l") 'spatial-navigate-forward-horizontal-box)
-(define-key evil-insert-state-map (kbd "C-k") 'spatial-navigate-backward-vertical-bar)
-(define-key evil-insert-state-map (kbd "C-j") 'spatial-navigate-forward-vertical-bar)
-(define-key evil-insert-state-map (kbd "C-h") 'spatial-navigate-backward-horizontal-bar)
-(define-key evil-insert-state-map (kbd "C-l") 'spatial-navigate-forward-horizontal-bar)
+(setq delete-by-moving-to-trash t)
+(setq trash-directory "~/.Trash")
+(setq org-archive-location (concat "archive/archive-"
+                                   (format-time-string "%Y%m" (current-time)) ".org_archive::"))
+(use-package org
+  :config
+    (setq org-log-repeat nil)
+)
+(use-package modeline
+  (setq doom-modeline-enable-word-count t))
 (after! git-gutter
   (setq git-gutter:update-interval 0.5))
 (advice-add 'evil-ex-search-next :after
@@ -104,3 +85,16 @@
         '((left-fringe . 5)
           (right-fringe . 5)))
   )
+(use-package spatial-navigate
+  :after (lsp-treemacs)
+  :ensure t)
+(with-eval-after-load 'lsp-treemacs
+  (doom-themes-treemacs-config))
+    (define-keyevil-normal-state-map (kbd "C-k") 'spatial-navigate-backward-vertical-box)
+    (define-key evil-normal-state-map (kbd "C-j") 'spatial-navigate-forward-vertical-box)
+    (define-key evil-normal-state-map (kbd "C-h") 'spatial-navigate-backward-horizontal-box)
+    (define-key evil-normal-state-map (kbd "C-l") 'spatial-navigate-forward-horizontal-box)
+    (define-key evil-insert-state-map (kbd "C-k") 'spatial-navigate-backward-vertical-bar)
+    (define-key evil-insert-state-map (kbd "C-j") 'spatial-navigate-forward-vertical-bar)
+    (define-key evil-insert-state-map (kbd "C-h") 'spatial-navigate-backward-horizontal-bar)
+    (define-key evil-insert-state-map (kbd "C-l") 'spatial-navigate-forward-horizontal-bar)
