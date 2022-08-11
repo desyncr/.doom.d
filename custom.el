b1d625068e9e63000ed4401e715ef2dda9ca9079
desyncr <des@riseup.net>
2022-08-11 19:17:35 +0100

    Update custom configuration from customize-group, customize-variable

--- custom.el
+++ custom.el
@@ -4,14 +4,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" default))
- '(flycheck-checker-error-threshold 10000)
- '(flycheck-phpcs-standard "psr12 ")
- '(lsp-file-watch-threshold 100000)
- '(magit-repository-directories '(("~/sys-vagrant/code/" . 1)))
  '(package-selected-packages
-   '(persp-projectile treemacs-projectile helm evil-multiedit multiple-cursors)))
+   '(treemacs-projectile shut-up persp-projectile helm evil-multiedit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
