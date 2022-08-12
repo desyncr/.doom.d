d10c51261eaa18a957ac25c76e8bd0e295f5ee04
desyncr <des@riseup.net>
2022-08-12 14:20:34 +0100

    Re-introduce customized variable values

--- custom.el
+++ custom.el
@@ -4,6 +4,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(flycheck-checker-error-threshold 10000)
+ '(lsp-file-watch-threshold 100000)
  '(package-selected-packages
    '(treemacs-projectile shut-up persp-projectile helm evil-multiedit)))
 '(flycheck-checker-error-threshold 10000)
