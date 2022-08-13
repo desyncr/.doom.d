6df353d134ead31d7bde655ce0003c127a10f783
DC* <des@riseup.net>
2022-08-13 14:07:00 +0100

    Commit compiled configuration

--- config.el
+++ config.el
@@ -1,21 +1,10 @@
 (setq user-full-name "DC*"
       user-mail-address "des@riseup.net")
-(require 'org-tempo)
-(add-to-list 'org-structure-template-alist '("sh" . "src shell"))
 (setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'medium))
 (setq doom-theme 'doom-nord-light)
 (setq display-line-numbers-type t)
 (setq org-directory "~/org/")
 (setq fancy-splash-image "~/.doom.d/splash/doom-emacs-bw-light.svg")
-(use-package yasnippet
-  :ensure t
-  :config (yas-global-mode 1))
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
-(package-initialize)
-
-(require 'yasnippet-snippets)
 (use-package blamer
   :bind (("s-i" . blamer-show-commit-info))
   :defer 20
@@ -42,12 +31,20 @@
   (super-save-mode +1))
 (map! :leader :desc "Open Dashboard" "d" #'+doom-dashboard/open)
 (map! :ne "M-/" #'comment-or-uncomment-region)
-(map! "s-b" #'ido-switch-buffer)
+(map! "s-b" #'ido-switch-buffer-other-window)
 (map! "s-t" #'+treemacs/toggle)
-(map! "s-§" #'+treemacs/toggle)
 (map! "s-s" #'save-buffer)
 (map! "s-f" #'+default/search-project)
 (map! "s-p" #'projectile-find-file)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(use-package yasnippet
+  :config (yas-global-mode 1))
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
+
+(require 'yasnippet-snippets)
 (after! projectile
    (setq
         projectile-project-search-path '("~/sys-vagrant/code/")
@@ -71,6 +68,45 @@
 (setq trash-directory "~/.Trash")
 (setq org-archive-location (concat "archive/archive-"
                                    (format-time-string "%Y%m" (current-time)) ".org_archive::"))
+(use-package org-modern
+  :config
+    ;; Add frame borders and window dividers
+    (modify-all-frames-parameters
+    '((right-divider-width . 5)
+    (internal-border-width . 5)))
+
+    (dolist (face '(window-divider
+                    window-divider-first-pixel
+                    window-divider-last-pixel))
+    (face-spec-reset-face face)
+    (set-face-foreground face (face-attribute 'default :background)))
+    (set-face-background 'fringe (face-attribute 'default :background))
+
+    (setq
+    ;; Edit settings
+    org-auto-align-tags nil
+    org-tags-column 0
+    org-catch-invisible-edits 'show-and-error
+    org-special-ctrl-a/e t
+    org-insert-heading-respect-content t
+
+    ;; Org styling, hide markup etc.
+    org-hide-emphasis-markers t
+    org-pretty-entities t
+    org-ellipsis "…"
+
+    ;; Agenda styling
+    org-agenda-tags-column 0
+    org-agenda-block-separator ?─
+    org-agenda-time-grid
+    '((daily today require-timed)
+    (800 1000 1200 1400 1600 1800 2000)
+    " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
+    org-agenda-current-time-string
+    "⭠ now ─────────────────────────────────────────────────")
+
+    (global-org-modern-mode)
+  )
 (use-package org
   :config
     (setq org-log-repeat nil)
