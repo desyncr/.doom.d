0d8e833ddc019a897bcd71671cc063cbd6698272
DC* <des@riseup.net>
2022-08-18 14:47:43 +0100

    Remove duplicate section Packages

--- packages.el
+++ packages.el
@@ -9,14 +9,3 @@
 (package! org-modern) ; Modern org UI
 (package! evil-snipe)
 (package! devdocs)
-(package! blamer)
-(package! deadgrep)
-(package! treemacs-projectile)
-(package! super-save)
-(package! better-jumper)
-(package! centered-cursor-mode)
-(package! vertico-posframe)
-(package! rainbow-delimiters)
-(package! org-modern) ; Modern org UI
-(package! evil-snipe)
-(package! devdocs)
