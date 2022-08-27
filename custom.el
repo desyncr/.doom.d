49328e1a41faa15915627dbdb617b9b48141ad3f
DC* <des@riseup.net>
2022-08-27 16:12:49 +0100

    Update custom.el configuraton

--- custom.el
+++ custom.el
@@ -1,15 +1,10 @@
-(put 'customize-variable 'disabled nil)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(lsp-file-watch-threshold 100000)
  '(package-selected-packages
-   '(yasnippet-snippets treemacs-projectile shut-up persp-projectile helm evil-multiedit)))
-'(flycheck-checker-error-threshold 10000)
-'(flycheck-phpcs-standard "psr12 ")
-'(lsp-file-watch-threshold 100000)
+   '(inf-ruby org-bullets yasnippet-snippets yascroll vterm treemacs-projectile shut-up persp-projectile helm evil-snipe evil-multiedit dirvish)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
