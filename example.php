f685dbf0b251344f3526e64db67ea80cb3c52690
DC* <des@riseup.net>
2022-09-13 20:41:29 +0100

    Commit compiled configuration

---
+++ example.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ *
+ */
+class Example
+{
+
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $this->expects($this->once())
+             ->method('')
+             ->with()
+             ->willReturn();
+
+    }
+}
