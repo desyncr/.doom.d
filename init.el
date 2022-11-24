03af744e88ce12f6df22b962b5635f1d4bd47f38
dario-exads <dario@exads.com>
2022-11-24 12:11:57 +0000

    [package] Enable indent-guides

--- init.el
+++ init.el
@@ -34,9 +34,9 @@
        ;;(emoji +unicode)    ; 🙂
        hl-todo             ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
-       ;indent-guides       ; highlighted indent columns
        ;(ligatures +fira)   ; ligatures and symbols to make your code pretty again
        ;minimap            ; show a map of the code on the side
+       indent-guides       ; highlighted indent columns
        modeline            ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
