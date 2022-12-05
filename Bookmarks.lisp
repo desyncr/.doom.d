99e09f71c8eba93ba708b93947ffbdcd0098f3f8
dario-exads <dario@exads.com>
2022-12-05 19:54:50 +0000

    [config] Configure bookmarks file to be stored outside emacs installation

---
+++ Bookmarks.lisp
@@ -0,0 +1,25 @@
+;;;; Emacs Bookmark Format Version 1;;;; -*- coding: utf-8-emacs; mode: lisp-data -*-
+;;; This format is meant to be slightly human-readable;
+;;; nevertheless, you probably don't want to edit it.
+;;; -*- End Of Bookmark File Format Version Stamp -*-
+(("Weekly"
+ (filename . "~/org/weekly.org")
+ (front-context-string . "* Tasks [2/10] :")
+ (rear-context-string . "| 0:25 |\n#+END:\n")
+ (position . 1300))
+("Queries"
+ (filename . "~/org/docs/queries/exads.org")
+ (front-context-string . ":PROPERTIES:\n:he")
+ (rear-context-string)
+ (position . 1))
+("Notes"
+ (filename . "~/org/notes.org")
+ (front-context-string . "box\n** [2022-10-")
+ (rear-context-string . "~SPC n n~.\n\n* In")
+ (position . 119))
+("Doom"
+ (filename . "~/.doom.d/Emacs.org")
+ (front-context-string . "* General\nThis s")
+ (rear-context-string . "[browser-url]]\n\n")
+ (position . 2480))
+)
