ad0a55975a457b74c1b329023de828246937e6af
dario-exads <dario@exads.com>
2022-12-13 13:44:35 +0000

    [custom] Update custom.el configuration

--- custom.el
+++ custom.el
@@ -4,17 +4,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(iwindow evil-nerd-commenter yascroll valign shell-pop ranger psysh ox-slack ob-sql-mode nano-theme move-text mixed-pitch minibar goto-line-preview flymake-phpstan flycheck-projectile flycheck-phpstan flycheck-php-noverify consult-lsp company-quickhelp company-php))
+   '(nov fancy-narrow defrepeater run-command major-mode-hydra pretty-hydra iwindow evil-nerd-commenter yascroll valign shell-pop ranger psysh ox-slack ob-sql-mode nano-theme move-text mixed-pitch minibar goto-line-preview flymake-phpstan flycheck-projectile flycheck-phpstan flycheck-php-noverify consult-lsp company-quickhelp company-php))
  '(warning-suppress-types '((initialization) (defvaralias) (org-element-cache))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(consult-grep-context ((t (:foreground "#AEBACF" :weight normal :slant normal))))
- '(consult-separator ((t (:foreground "#AEBACF" :weight normal :slant normal))))
+ '(consult-grep-context ((t (:foreground "#aebacf" :weight normal :slant normal))))
+ '(consult-separator ((t (:foreground "#aebacf" :weight normal :slant normal))))
  '(line-number ((t (:foreground "#6f7787" :weight normal :slant normal))))
- '(line-number-current-line ((t (:foreground "#AEBACF" :weight normal :slant normal))))
+ '(line-number-current-line ((t (:foreground "#aebacf" :weight normal :slant normal))))
  '(org-block ((t (:inherit fixed-pitch))))
  '(org-code ((t (:inherit (shadow fixed-pitch)))))
  '(org-document-info ((t (:foreground "dark orange"))))
