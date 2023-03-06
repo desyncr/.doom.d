75f449acff14e277cd8227e2c883ba9f957d831d
dario-exads <dario@exads.com>
2023-03-06 09:42:12 +0000

    main: Update php example / playground

--- example.php
+++ example.php
@@ -5,16 +5,39 @@
  */
 class Example
 {
+    /** @var string */
+    private $var;
 
     /**
      *
      */
     public function __construct()
     {
-        $this->expects($this->once())
-             ->method('')
-             ->with()
-             ->willReturn();
-
     }
+
+    /**
+     * Returns an array of randomly generated integers.
+     *
+     * @param int $count
+     * @return array
+     */
+    public function getRandomNumbersIterative(int $count): array
+    {
+        $result = [];
+        for (; $count >= 0; $count--) {
+            array_push($result, chr(rand(48, 56)));
+        }
+
+        return $result;
+    }
+
+    public function printRandomNumbers(): void
+    {
+        echo join(', ', $this->getRandomNumbersIterative(10)) . PHP_EOL;
+    }
+
 }
+
+
+$example = new Example();
+$example->printRandomNumbers();
