2292455dfdcfada765ebb662ee3ddf6685fa7928
DC* <des@riseup.net>
2023-03-18 12:43:39 +0000

    main: Disable few packages

--- packages.el
+++ packages.el
@@ -12,26 +12,26 @@
 
 (package! browse-at-remote)
 
-(package! expand-region) ;; See repository [[https://github.com/magnars/expand-region.el][here]].
+;(package! expand-region) ;; See repository [[https://github.com/magnars/expand-region.el][here]].
 (package! rainbow-delimiters)
 (package! consult-yasnippet) ;; Consult integration with yasnippet:
 (package! devdocs)
-(package! better-jumper)
+;(package! better-jumper)
 (package! flycheck-phpstan)
 
-(package! evil-snipe :disable t) ;; Search in buffer:
+;(package! evil-snipe :disable t) ;; Search in buffer:
 (package! evil-nerd-commenter)
 (package! move-text)
 
 (package! org-modern)
-(package! org-auto-tangle)
+;(package! org-auto-tangle)
 (package! inf-ruby)
-(package! ob-async)
-(package! ob-http)
+;(package! ob-async)
+;(package! ob-http)
 (package! consult-org-roam)
 (package! org-bullets)
 (package! toc-org)
-(package! literate-calc-mode)
+;(package! literate-calc-mode)
 (package! ox-slack)
 
 (package! keyfreq)
