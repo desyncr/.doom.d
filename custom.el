52974cc92ce3c3c2f7ee45d33505d9b7938e4074
DC* <des@riseup.net>
2023-03-18 12:41:26 +0000

    main: Update custom

--- custom.el
+++ custom.el
@@ -3,27 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(diff-hl nov fancy-narrow defrepeater run-command major-mode-hydra pretty-hydra iwindow evil-nerd-commenter yascroll valign shell-pop ranger psysh ox-slack ob-sql-mode nano-theme move-text mixed-pitch minibar goto-line-preview flymake-phpstan flycheck-projectile flycheck-phpstan flycheck-php-noverify consult-lsp company-quickhelp company-php))
- '(warning-suppress-types '((initialization) (defvaralias) (org-element-cache))))
+ '(warning-suppress-log-types '((initialization) (defvaralias))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(consult-grep-context ((t (:foreground "#aebacf" :weight normal :slant normal))))
- '(consult-separator ((t (:foreground "#aebacf" :weight normal :slant normal))))
- '(line-number ((t (:foreground "#6f7787" :weight normal :slant normal))))
- '(line-number-current-line ((t (:foreground "#aebacf" :weight normal :slant normal))))
- '(org-block ((t (:inherit fixed-pitch))))
- '(org-code ((t (:inherit (shadow fixed-pitch)))))
- '(org-document-info ((t (:foreground "dark orange"))))
- '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
- '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
- '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- '(org-modern-block-name ((t nil)))
- '(org-property-value ((t (:inherit fixed-pitch))) t)
- '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
- '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold))))
- '(org-verbatim ((t (:inherit (shadow fixed-pitch))))))
+ )
