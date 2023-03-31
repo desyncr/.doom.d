ed9ba29b05cf2417328022bad88091fa0c207b12
dario-exads <dario@exads.com>
2023-03-31 19:08:20 +0100

    main: Update bookmarks

--- bookmarks.el
+++ bookmarks.el
@@ -4,27 +4,17 @@
 ;;; -*- End Of Bookmark File Format Version Stamp -*-
 (("Weekly"
  (filename . "~/Notes/weekly.org")
- (front-context-string . "* Tasks [2/10] :")
- (rear-context-string . "| 0:25 |\n#+END:\n")
- (position . 1300))
+ (position . 0))
 ("Commands"
  (filename . "~/Notes/docs/commands.org")
- (front-context-string . "")
- (rear-context-string . "")
  (position . 0))
 ("Notes"
  (filename . "~/Personal/notes.org")
- (front-context-string . "box\n** [2022-10-")
- (rear-context-string . "~SPC n n~.\n\n* In")
- (position . 119))
+ (position . 0))
 ("Journal"
  (filename . "~/Personal/journal.org")
- (front-context-string . "")
- (rear-context-string . "")
  (position . 0))
 ("Config"
  (filename . "~/.config/doom/Emacs.org")
- (front-context-string . "* Forewords\nThis")
- (rear-context-string . "nse][License]]\n\n")
- (position . 580))
+ (position . 0))
 )
