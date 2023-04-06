92ae916d5a09e7472c0aab5c61863d649b5b51f9
dario-exads <dario@exads.com>
2023-04-06 13:41:05 +0100

    main: Remove duplicate bookmarks from configuration

--- bookmarks.el
+++ bookmarks.el
@@ -20,22 +20,4 @@
 ("Doom Config"
  (filename . "~/.config/doom/Emacs.org")
  (position . 0))
-("Weekly<2>"
- (filename . "~/Notes/weekly.org")
- (position . 0))
-("Commands<2>"
- (filename . "~/Notes/docs/commands.org")
- (position . 0))
-("Notes<2>"
- (filename . "~/Notes/notes.org")
- (position . 0))
-("Personal<2>"
- (filename . "~/Personal/notes.org")
- (position . 0))
-("Journal<2>"
- (filename . "~/Personal/journal.org")
- (position . 0))
-("Doom Config<2>"
- (filename . "~/.config/doom/Emacs.org")
- (position . 0))
 )
