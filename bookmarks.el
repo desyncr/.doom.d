4465a9ba88a57f3bcb093239acb8f0e571e1c08c
dario-exads <dario@exads.com>
2023-04-06 13:01:57 +0100

    main: Update bookmarks

--- bookmarks.el
+++ bookmarks.el
@@ -9,12 +9,33 @@
  (filename . "~/Notes/docs/commands.org")
  (position . 0))
 ("Notes"
+ (filename . "~/Notes/notes.org")
+ (position . 0))
+("Personal"
  (filename . "~/Personal/notes.org")
  (position . 0))
 ("Journal"
  (filename . "~/Personal/journal.org")
  (position . 0))
-("Config"
+("Doom Config"
+ (filename . "~/.config/doom/Emacs.org")
+ (position . 0))
+("Weekly<2>"
+ (filename . "~/Notes/weekly.org")
+ (position . 0))
+("Commands<2>"
+ (filename . "~/Notes/docs/commands.org")
+ (position . 0))
+("Notes<2>"
+ (filename . "~/Notes/notes.org")
+ (position . 0))
+("Personal<2>"
+ (filename . "~/Personal/notes.org")
+ (position . 0))
+("Journal<2>"
+ (filename . "~/Personal/journal.org")
+ (position . 0))
+("Doom Config<2>"
  (filename . "~/.config/doom/Emacs.org")
  (position . 0))
 )
