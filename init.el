3417de1a3d7ce03f9849b9e39bb86e165a8089ad
DC* <des@riseup.net>
2023-04-08 01:52:15 +0100

    main: Enable syntax, spell and grammar doom modules

--- init.el
+++ init.el
@@ -79,9 +79,9 @@
        vterm               ; the best terminal emulation in Emacs
 
        :checkers
-       syntax              ; tasing you for every semicolon you forget
-       ;(spell +flyspell)   ; tasing you for misspelling mispelling
-       ;grammar             ; tasing grammar mistake every you make
+       (syntax +childframe); tasing you for every semicolon you forget
+       (spell +flyspell)   ; tasing you for misspelling mispelling
+       grammar             ; tasing grammar mistake every you make
 
        :tools
        ;;ansible
