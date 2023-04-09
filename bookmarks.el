2cc599a3b2f3631efad1d75b25ef8958f65e7f4c
DC* <des@riseup.net>
2023-04-09 19:26:40 +0100

    main: Update bookmarks

--- bookmarks.el
+++ bookmarks.el
@@ -2,22 +2,30 @@
 ;;; This format is meant to be slightly human-readable;
 ;;; nevertheless, you probably don't want to edit it.
 ;;; -*- End Of Bookmark File Format Version Stamp -*-
-(("Weekly"
- (filename . "~/Work/weekly.org")
- (position . 0))
-("Commands"
- (filename . "~/Work/docs/commands.org")
- (position . 0))
-("Work"
- (filename . "~/Work/notes.org")
- (position . 0))
-("Personal"
- (filename . "~/Personal/notes.org")
- (position . 0))
-("Journal"
- (filename . "~/Personal/journal.org")
- (position . 0))
-("Doom Config"
- (filename . "~/.config/doom/Emacs.org")
- (position . 0))
-)
+(;; Personal bookmarks
+ ("Personal > Notes"
+  (filename . "~/Personal/notes.org"))
+("Personal > Writing"
+  (filename . "~/Personal/personal.org"))
+("Personal > TODOs"
+  (filename . "~/Personal/todo.org"))
+("Personal > Projects"
+  (filename . "~/Personal/projects.org"))
+("Personal > Journal"
+  (filename . "~/Personal/journal.org"))
+;; Work bookmarks
+("Work > Notes"
+  (filename . "~/Work/work.org"))
+("Work > TODOs"
+  (filename . "~/Work/tasks.org"))
+("Work > Weekly"
+  (filename . "~/Work/weekly.org"))
+("Work > Commands"
+  (filename . "~/Work/docs/commands.org"))
+;; Config bookmarks
+("Doom > Config"
+  (filename . "~/.config/doom/Emacs.org"))
+("Doom > Bookmarks"
+  (filename . "~/.config/doom/bookmarks.el"))
+("Doom > Init.el"
+  (filename . "~/.config/doom/init.el")))
