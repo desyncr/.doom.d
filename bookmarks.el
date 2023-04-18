18b017b2f6a05cbf06e9a099be7964629f13acac
dario-exads <dario@exads.com>
2023-04-18 23:49:51 +0100

    main: Update bookmarks

--- bookmarks.el
+++ bookmarks.el
@@ -3,25 +3,19 @@
 ;;; nevertheless, you probably don't want to edit it.
 ;;; -*- End Of Bookmark File Format Version Stamp -*-
 (;; Personal bookmarks
- ("Personal > Notes"
-  (filename . "~/Personal/notes.org"))
-("Personal > Writing"
-  (filename . "~/Personal/personal.org"))
-("Personal > TODOs"
+ ("Personal > TODOs"
   (filename . "~/Personal/todo.org"))
+ ("Personal > Emacs"
+  (filename . "~/Personal/emacs.org"))
 ("Personal > Projects"
   (filename . "~/Personal/projects.org"))
 ("Personal > Journal"
   (filename . "~/Personal/journal.org"))
 ;; Work bookmarks
-("Work > Notes"
-  (filename . "~/Work/work.org"))
 ("Work > TODOs"
   (filename . "~/Work/tasks.org"))
-("Work > Weekly"
-  (filename . "~/Work/weekly.org"))
 ("Work > Commands"
-  (filename . "~/Work/docs/commands.org"))
+  (filename . "~/Work/notes/commands.org"))
 ;; Config bookmarks
 ("Doom > Config"
   (filename . "~/.config/doom/Emacs.org"))
