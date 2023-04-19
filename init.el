93aad0b8b6dff1209d3e5cec55d61a4001f1285c
DC* <des@riseup.net>
2023-04-19 23:26:16 +0100

    main: Jumping back into doom emacs popup module

--- init.el
+++ init.el
@@ -41,7 +41,7 @@
        ;;nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints             ; highlight the region an operation acts on
-       (popup)             ; tame sudden yet inevitable temporary windows
+       (popup +all +defaults)             ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
        treemacs            ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
@@ -69,7 +69,7 @@
        (dired +ranger +icons)     ; making dired pretty [functional]
        electric            ; smarter, keyword-based electric-indent
        ibuffer             ; interactive buffer management
-       (undo +tree)        ; persistent, smarter undo for your inevitable mistakes
+       undo                ; persistent, smarter undo for your inevitable mistakes
        vc                  ; version-control and Emacs, sitting in a tree
 
        :term
