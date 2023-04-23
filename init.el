c1ab9e8312bd4521ea50fee55aceaf48f925cbd2
DC* <des@riseup.net>
2023-04-23 02:53:20 +0100

    main: Back to org-roam

--- init.el
+++ init.el
@@ -152,7 +152,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +roam2 +journal)        ; organize your plain life in plain text
+       (org +roam2 +journal +roam2 +present +dragndrop); organize your plain life in plain text
        (php +lsp)          ; perl's insecure younger brother
        plantuml            ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
