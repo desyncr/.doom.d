d86c2a4edb341aaec897039a2ecc4ff7989b04f0
DC* <des@riseup.net>
2023-09-01 17:04:35 +0100

    main: Update bookmarks

--- bookmarks.el
+++ bookmarks.el
@@ -2,26 +2,24 @@
 ;;; This format is meant to be slightly human-readable;
 ;;; nevertheless, you probably don't want to edit it.
 ;;; -*- End Of Bookmark File Format Version Stamp -*-
-(;; Personal bookmarks
- ("Personal > TODOs"
-  (filename . "~/Personal/todo.org"))
- ("Personal > Emacs"
-  (filename . "~/Personal/emacs.org"))
+(("Personal > TODOs"
+ (filename . "~/Personal/todo.org"))
+("Personal > Emacs"
+ (filename . "~/Personal/emacs.org"))
 ("Personal > Projects"
-  (filename . "~/Personal/projects.org"))
+ (filename . "~/Personal/projects.org"))
 ("Personal > Journal"
-  (filename . "~/Personal/journal.org"))
-;; Work bookmarks
+ (filename . "~/Personal/journal.org"))
 ("Work > TODOs"
-  (filename . "~/Work/tasks.org"))
+ (filename . "~/Work/tasks.org"))
 ("Work > Commands"
-  (filename . "~/Work/notes/commands.org"))
-;; Config bookmarks
+ (filename . "~/Work/notes/commands.org"))
 ("Doom > Config"
-  (filename . "~/.config/doom/Emacs.org"))
+ (filename . "~/.config/doom/Emacs.org"))
 ("Doom > Bookmarks"
-  (filename . "~/.config/doom/bookmarks.el"))
+ (filename . "~/.config/doom/bookmarks.el"))
 ("Doom > Packages"
-  (filename . "~/.config/doom/config/packages.org"))
+ (filename . "~/.config/doom/config/packages.org"))
 ("Doom > Init.el"
-  (filename . "~/.config/doom/init.el")))
+ (filename . "~/.config/doom/init.el"))
+)
